f6b727dbfb2090c997345ab014d67cd735157597
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-23 15:52:33 +0530

    Add project files.

---
+++ EmployeePayrollServices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Service");
+        }
+    }
+}
